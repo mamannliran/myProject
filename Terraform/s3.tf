c697c5b5e7ecf18c540727d515c0d3ea2c75c4dc
liran <mamannliran@gmail.com>
2024-07-12 11:48:34 +0300

    commit5

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -11,3 +11,11 @@ resource "aws_s3_bucket" "jenkins-config" {
   bucket = "liran13-jenkins-config"
   acl = "private"
 }
+
+resource "aws_s3_object" "jenkins-config" {
+  bucket = aws_s3_bucket.jenkins-config.id
+  for_each = fileset("jenkins-config/", "*")
+  key = each.value
+  source = "jenkins-config/${each.value}"
+  etag = filemd5("jenkins-config/${each.value}")
+}
